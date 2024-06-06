46357c694b7d307421e7422cd86968b317e888ca
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-06 12:27:55 +0200

    ADD: delete

--- resources/views/Admin/Projects/index.blade.php
+++ resources/views/Admin/Projects/index.blade.php
@@ -19,8 +19,15 @@
       <td><a href="{{route('admin.projects.show', $item)}}">{{$item->title}}</a></td>
       <td>{{$item->type->type}}</td>
       <td>{{$item->description}}</td>
-      <td>
+      <td class="d-flex gap-3 align-items-center justify-content-center">
         <a href="{{route('admin.projects.edit', $item)}}">Modifica</a>
+        
+        <form action="{{route('admin.projects.destroy', $item)}}" method="POST">
+         @csrf
+         @method('DELETE')
+           <button class="btn text-danger">Elimina</button>
+        
+        </form>
       </td>
     </tr>
     @endforeach
