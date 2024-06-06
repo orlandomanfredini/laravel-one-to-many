4edbcd62a2ee8680e35f6bdf94a71f24b37f817f
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-06 15:28:58 +0200

    Commit

--- resources/views/Admin/Projects/create.blade.php
+++ resources/views/Admin/Projects/create.blade.php
@@ -24,13 +24,26 @@
     <div class="mb-3">
         <select name="type_id" id="type_id">
             <option value="">Seleziona Tipo Di Progetto</option>
-            @foreach ( $types as $type )
-             <option value="{{$type->id}}">{{$type->slug}}</option>
+            @foreach ($types as $type)
+                <option @selected($type->id == old('type_id') ) value="{{$type->id}}">{{$type->slug}}</option>
             @endforeach
         </select>
     </div>
 
     <button class="btn btn-primary">Crea</button>
 </form>
+@if ($errors->any())
+    <div class="alert alert-danger mt-5 col">
+        <ul>
+            @foreach ($errors->all() as $error)
+
+                <li>
+                    {{$error}}
+                </li>
+
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 @endsection
