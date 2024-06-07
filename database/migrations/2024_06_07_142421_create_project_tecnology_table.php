c02595212c7b54fdbd8d8c86a4aad0fcd7421bb7
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-07 16:41:08 +0200

    ADD: migration project/tecnology

---
+++ database/migrations/2024_06_07_142421_create_project_tecnology_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_tecnology', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id');
+            $$table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+
+            $table->unsignedBigInteger('tecnolgy_id');
+            $table->foreign('tecnology_id')->references('id')->on('tecnologies')->onDelete('cascade');
+
+            $table->primary('project_id', 'tecnology_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_tecnology');
+    }
+};
